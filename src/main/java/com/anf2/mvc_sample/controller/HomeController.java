1bd4977b20b2d9d858036b8d7ccd0e287f54fea2
stepan <stepan.anfinogenov@gmail.com>
2023-02-01 15:59:14 +0000

    2_simple form for creating new Employee

--- src/main/java/com/anf2/mvc_sample/controller/HomeController.java
+++ src/main/java/com/anf2/mvc_sample/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.anf2.mvc_sample.controller;
 
+import com.anf2.mvc_sample.model.Employee;
 import com.anf2.mvc_sample.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
     private EmployeeRepository employeeRepository;
 
@@ -15,10 +19,19 @@ public class HomeController {
         this.employeeRepository = employeeRepository;
     }
 
-    @RequestMapping("/")
+    @GetMapping
     public String getIndex(Model model) {
         model.addAttribute("employees", employeeRepository.findAll());
+        model.addAttribute("newemployee", new Employee());
 
         return "index";
     }
+
+    @PostMapping
+    public String createEmployee(Employee employee) {
+        employeeRepository.save(employee);
+
+        return "redirect:/";
+    }
+
 }
